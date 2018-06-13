1a53d36d67c742b97ca98ba439fc47e5374e836f
Ryan Murphy <murjax@gmail.com>
2018-06-12 21:05:53 -0400

    Add character to morse translations

--- spec/translate_morse_spec.rb
+++ spec/translate_morse_spec.rb
@@ -10,4 +10,55 @@ describe TranslateMorse do
       expect(translate_morse.text).to eq(text)
     end
   end
+
+  describe 'letter_translation_table' do
+    it 'returns hash of letter to morse translations' do
+      translations = { a: '.-',
+                       b: '-...',
+                       c: '-.-.',
+                       d: '-..',
+                       e: '.',
+                       f: '..-.',
+                       g: '--.',
+                       h: '....',
+                       i: '..',
+                       j: '.---',
+                       k: '-.-',
+                       l: '.-..',
+                       m: '--',
+                       n: '-.',
+                       o: '---',
+                       p: '.--.',
+                       q: '--.-',
+                       r: '.-.',
+                       s: '...',
+                       t: '-',
+                       u: '..-',
+                       v: '...-',
+                       w: '.--',
+                       x: '-..-',
+                       y: '-.--',
+                       z: '--..',
+      }
+      expect(translate_morse.letter_translation_table).to eq(translations)
+    end
+  end
+
+  describe 'number_translation_table' do
+    it 'returns array of number translations indexed in order' do
+      translations = [
+        '.----',
+        '..---',
+        '...--',
+        '....-',
+        '.....',
+        '-....',
+        '--...',
+        '---..',
+        '----.',
+        '-----',
+      ]
+      expect(translate_morse.number_translation_table).to eq(translations)
+    end
+  end
 end
